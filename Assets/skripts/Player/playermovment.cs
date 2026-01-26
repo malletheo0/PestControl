341aa079040579fd8684c14a7e6d51b657ddc4a7
malletheo0 <theo.mallet@elev.ga.lbs.se>
2026-01-26 13:07:01 +0100

    Update playermovment.cs

--- Assets/skripts/Player/playermovment.cs
+++ Assets/skripts/Player/playermovment.cs
@@ -2,27 +2,15 @@ using UnityEngine;
 
 public class playermovment : MonoBehaviour
 {
-    public float moveSpeed = 5f; // Movement speed
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
 
+    // Update is called once per frame
     void Update()
     {
-        float moveX = 0f;
-        float moveY = 0f;
-
-        // Check for input
-        if (Input.GetKey(KeyCode.W))
-            moveY = 1f;
-        if (Input.GetKey(KeyCode.S))
-            moveY = -1f;
-        if (Input.GetKey(KeyCode.A))
-            moveX = -1f;
-        if (Input.GetKey(KeyCode.D))
-            moveX = 1f;
-
-        // Normalize movement vector to prevent faster diagonal movement
-        Vector3 move = new Vector3(moveX, moveY, 0f).normalized;
-
-        // Move the player
-        transform.position += move * moveSpeed * Time.deltaTime;
+        
     }
 }
