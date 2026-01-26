57f63e196f773444025d64476854478567135f78
gislander08 <victor.gislander@elev.ga.lbs.se>
2026-01-26 14:45:14 +0100

    start raycast
    wow inte alls passivt aggresivt

--- Assets/skripts/level/wallstop.cs
+++ Assets/skripts/level/wallstop.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 public class wallstop : MonoBehaviour
 {
 
+    public Transform top;
+    public Transform left;
+    public Transform right;
+    public Transform bottom;
+    public Vector3 speed = new Vector3();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,5 +19,14 @@ public class wallstop : MonoBehaviour
     {
         
     }
+
+    private void FixedUpdate()
+    {
+        if(speed.x >= 0)
+        {
+            RaycastHit2D rightCast = Physics2D.Raycast(top.position + Vector3.up * 0.2f, Vector3.up, Mathf.Abs(speed.x * Time.fixedDeltaTime));
+        }
+    }
+
 
 }
