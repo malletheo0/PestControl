95887f6a3f589dada362b94fa2d587fde57d4262
gislander08 <victor.gislander@elev.ga.lbs.se>
2026-02-09 09:59:45 +0100

    levelSelector
    vrf

---
+++ Assets/Scripts/Button/LevelSelector.cs
@@ -0,0 +1,99 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void level1()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void level2()
+    {
+        SceneManager.LoadScene(2);
+
+    }
+    public void level3()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void level4()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void level5()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void level6()
+    {
+        SceneManager.LoadScene(7);
+    }
+    public void level7()
+    {
+        SceneManager.LoadScene(7);
+    }
+    public void level8()
+    {
+        SceneManager.LoadScene(8);
+    }
+    public void level9()
+    {
+        SceneManager.LoadScene(9);
+    }
+    public void level10()
+    {
+        SceneManager.LoadScene(10);
+    }
+    public void level11()
+    {
+        SceneManager.LoadScene(11);
+    }
+    public void level12()
+    {
+        SceneManager.LoadScene(12);
+    }
+    public void level13()
+    {
+        SceneManager.LoadScene(13);
+    }
+    public void level14()
+    {
+        SceneManager.LoadScene(14);
+    }
+    public void level15()
+    {
+        SceneManager.LoadScene(15);
+    }
+    public void level16()
+    {
+        SceneManager.LoadScene(16);
+    }
+    public void level17()
+    {
+        SceneManager.LoadScene(17);
+    }
+    public void level18()
+    {
+        SceneManager.LoadScene(18);
+    }
+    public void level19()
+    {
+        SceneManager.LoadScene(19);
+    }
+    public void level20()
+    {
+        SceneManager.LoadScene(20);
+    }
+}
