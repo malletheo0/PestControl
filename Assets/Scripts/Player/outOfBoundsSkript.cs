c8693565cb1c3588dc4f136d8390d217d14173d0
gislander08 <victor.gislander@elev.ga.lbs.se>
2026-02-10 14:13:38 +0100

    fixade med tril
    gaga

--- Assets/Scripts/Player/outOfBoundsSkript.cs
+++ Assets/Scripts/Player/outOfBoundsSkript.cs
@@ -4,9 +4,16 @@ using UnityEngine.SceneManagement;
 public class outOfBoundsSkript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject fade;
+    public GameObject player;
+    public GameObject canvas;
+
+    public Vector3 fadeStartPosition;
+    public Vector3 playerStartPosition;
     void Start()
     {
-        
+        playerStartPosition = player.transform.position;
+        fadeStartPosition = fade.transform.position;
     }
 
     // Update is called once per frame
@@ -19,8 +26,10 @@ public class outOfBoundsSkript : MonoBehaviour
     {
         if(collision.gameObject.tag == "Player")
         {
-            int id = SceneManager.GetActiveScene().buildIndex;
-            SceneManager.LoadScene(id);
+            player.transform.position = playerStartPosition;
+            Instantiate(fade, fadeStartPosition, Quaternion.identity.normalized,canvas.transform);
+        //    int id = SceneManager.GetActiveScene().buildIndex;
+        //    SceneManager.LoadScene(id);
         }
         else if(collision.gameObject.tag == "Block")
         {
