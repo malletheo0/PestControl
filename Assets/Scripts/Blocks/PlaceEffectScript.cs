92be43d39c43234e1531d8490545dc75021d4df8
gislander08 <victor.gislander@elev.ga.lbs.se>
2026-02-12 10:24:01 +0100

    smoke men dålig
    agfawf

--- Assets/Scripts/Blocks/PlaceEffectScript.cs
+++ Assets/Scripts/Blocks/PlaceEffectScript.cs
@@ -4,9 +4,11 @@ public class PlaceEffectScript : MonoBehaviour
 {
     public AudioSource audioSource;
     public AudioClip placeSound;
+    public ParticleSystem smoke;
     void Start()
     {
         audioSource.PlayOneShot(placeSound);
+        Instantiate(smoke, transform.position, Quaternion.Euler(90,0,0));
     }
 
     void Update()
