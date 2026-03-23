dccf3fbb3e6f92e071126fa0dd8a260572166a4e
gislander08 <victor.gislander@elev.ga.lbs.se>
2026-03-23 14:29:33 +0100

    level selector color
    wooo

--- Assets/Scripts/Button/LevelSelector.cs
+++ Assets/Scripts/Button/LevelSelector.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelSelector : MonoBehaviour
 {
@@ -7,12 +8,22 @@ public class LevelSelector : MonoBehaviour
     public GameObject soundManager;
     public int levelUnlock;
     public AudioSource audioSource;
+    public Image[] levelButtons;
 
     void Start()
     {
         soundManager = GameObject.FindGameObjectWithTag("SoundManager");
         levelUnlock = soundManager.GetComponent<SoundManagerScript>().highestLevelNumber;
         audioSource = GameObject.FindGameObjectWithTag("SoundManager").GetComponent<AudioSource>();
+
+        for (int i = 0; i < levelButtons.Length; i++)
+        {
+            if(i + 1 > levelUnlock)
+            {
+                levelButtons[i].color = Color.gray;
+            }
+        }
+        
     }
 
     // Update is called once per frame
